e33d75d7aceab06a272f02988f61310ac5e48cf6
jay menorca <zaimenorca@gmail.com>
2024-01-25 07:09:04 -0500

    75; 1st commit for jpa-hibernate-demo

---
+++ jpa-hibernate-demo/src/main/java/jayslabs/jpahibernatedemo/JpaHibernateDemoApplication.java
@@ -0,0 +1,13 @@
+package jayslabs.jpahibernatedemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JpaHibernateDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JpaHibernateDemoApplication.class, args);
+	}
+
+}
