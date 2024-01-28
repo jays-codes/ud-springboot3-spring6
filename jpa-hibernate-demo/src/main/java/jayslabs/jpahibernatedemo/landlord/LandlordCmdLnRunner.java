0db86f1ab516accca989f949837ebcfcecf71d86
jay menorca <zaimenorca@gmail.com>
2024-01-28 17:26:25 -0500

    83; JpaRepository count(); custom findBy()

--- jpa-hibernate-demo/src/main/java/jayslabs/jpahibernatedemo/landlord/LandlordCmdLnRunner.java
+++ jpa-hibernate-demo/src/main/java/jayslabs/jpahibernatedemo/landlord/LandlordCmdLnRunner.java
@@ -18,15 +18,19 @@ public class LandlordCmdLnRunner implements CommandLineRunner{
 	
 	@Override
 	public void run(String... args) throws Exception {
-		repo.save(new Landlord(4,"Chopah","Property X1234512"));
-		repo.save(new Landlord(2,"Luffy","Property X1246734"));
-		repo.save(new Landlord(3,"Robeen","Property X1259156"));
+		repo.save(new Landlord(4,"Chopah","Property X12345128"));
+		repo.save(new Landlord(2,"Luffy","Property X12467348"));
+		repo.save(new Landlord(3,"Robeen","Property X12591568"));
+		repo.save(new Landlord(5,"Sanji","Property S12491228"));
+		repo.save(new Landlord(6,"Zoro","Property S777777"));
+
 		
 		repo.deleteById(2l);
 		System.out.println(repo.findById(3l));
 		
+		System.out.println(repo.findByName("Sanji"));
 //		Landlord ll = repo.findById(3l);
-//		System.out.println(ll);
+		System.out.println("count: " + repo.count());
 	}
 
 }
--- jpa-hibernate-demo/src/main/java/jayslabs/jpahibernatedemo/landlord/LandlordSpringDataJpaRepository.java
+++ jpa-hibernate-demo/src/main/java/jayslabs/jpahibernatedemo/landlord/LandlordSpringDataJpaRepository.java
@@ -1,7 +1,10 @@
 package jayslabs.jpahibernatedemo.landlord;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface LandlordSpringDataJpaRepository extends JpaRepository<Landlord, Long> {
-
+	List<Landlord> findByName(String name);
+	
 }
