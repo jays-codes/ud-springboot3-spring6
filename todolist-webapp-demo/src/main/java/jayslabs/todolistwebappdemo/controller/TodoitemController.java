a9188910d69bf4fa6028b4c987976beec122bc31
jay menorca <zaimenorca@gmail.com>
2024-01-28 21:05:54 -0500

    94; added jsp; view resolvers in app.prop; tomcat-embed-jasper dep

--- todolist-webapp-demo/src/main/java/jayslabs/todolistwebappdemo/controller/TodoitemController.java
+++ todolist-webapp-demo/src/main/java/jayslabs/todolistwebappdemo/controller/TodoitemController.java
@@ -12,4 +12,26 @@ public class TodoitemController {
 	public String sayHello() {
 		return "Hello, what's up?";
 	}
+	
+	@RequestMapping("hellohtml")
+	@ResponseBody
+	public String sayHelloHtml() {
+		StringBuffer sb = new StringBuffer();
+		sb.append("<html>");
+		sb.append("<head>");
+		sb.append("<title>to do application</title>");
+		sb.append("</head>");
+		sb.append("<body>");
+		sb.append("body - todo application");
+		sb.append("</body>");
+		sb.append("</html>");
+		return sb.toString();
+	}
+	
+	//hello jsp
+	//src\main\resources\META-INF\resources\WEB-INF\jsp\hello.jsp
+	@RequestMapping("hellojsp")
+	public String helloJsp() {
+		return "hello";
+	}
 }
