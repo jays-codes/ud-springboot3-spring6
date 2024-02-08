d399778f8a73b5b01a27d1e8bc0101ee7e47ca02
jay menorca <zaimenorca@gmail.com>
2024-02-07 19:18:46 -0500

    150; implemented POST api; tested via postman

--- restful-web-services/src/main/java/jayslabs/restfulwebservices/user/UserDAOService.java
+++ restful-web-services/src/main/java/jayslabs/restfulwebservices/user/UserDAOService.java
@@ -6,28 +6,35 @@ import java.util.List;
 
 import org.springframework.stereotype.Component;
 
-
 @Component
 public class UserDAOService {
 //findalluser, save, finduser
 	private static List<User> users = new ArrayList<>();
-	static {
-		users.add(new User(1,"jay", LocalDate.now().minusYears(47)));
-		users.add(new User(2,"ethan", LocalDate.now().minusYears(15)));
-		users.add(new User(3,"sette", LocalDate.now().minusYears(40)));
+	private static int userCount = 3;
 
+	static {
+		users.add(new User(++userCount, "jay", LocalDate.now().minusYears(47)));
+		users.add(new User(++userCount, "ethan", LocalDate.now().minusYears(15)));
+		users.add(new User(++userCount, "sette", LocalDate.now().minusYears(40)));
 	}
+	
 	public List<User> findAll() {
 		return users;
 	}
+
+	public User save(User user) {
+		user.setId(++userCount);
+		users.add(user);
+		return user;
+	}
+	
 	public User findById(int id) {
-		//Todo td = todos.stream().filter(t -> t.getId()==id).findFirst().get();//.orElse(null);
+		// Todo td = todos.stream().filter(t ->
+		// t.getId()==id).findFirst().get();//.orElse(null);
 
 		User user = users.stream().filter(u -> u.getId().equals(id)).findFirst().get();
 		// TODO Auto-generated method stub
 		return user;
 	}
-	
-	
 
 }
