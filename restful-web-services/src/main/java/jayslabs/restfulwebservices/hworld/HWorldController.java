e221b8882f626269a8e7a7817ff1b1fec9433142
jay menorca <zaimenorca@gmail.com>
2024-02-09 14:58:55 -0500

    163; implemented i18n; messages.properties, MessageSource, getMessages(), Locale, getLocale()

--- restful-web-services/src/main/java/jayslabs/restfulwebservices/hworld/HWorldController.java
+++ restful-web-services/src/main/java/jayslabs/restfulwebservices/hworld/HWorldController.java
@@ -1,5 +1,9 @@
 package jayslabs.restfulwebservices.hworld;
 
+import java.util.Locale;
+
+import org.springframework.context.MessageSource;
+import org.springframework.context.i18n.LocaleContextHolder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -7,6 +11,12 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HWorldController {
 	
+	private MessageSource msgSource;
+	
+	public HWorldController(MessageSource msgSource) {
+		this.msgSource = msgSource;
+	}
+
 	@GetMapping(path="/yow")
 	public String helloAction() {
 		return "Wazzzzzz upppppp!!!";
@@ -20,5 +30,13 @@ public class HWorldController {
 	@GetMapping(path="/hello/pathparam/{name}")
 	public HWorld getHelloWithPathVar(@PathVariable String name) {
 		return new HWorld("Imma Bean Yow! Ma nem is: " + name);
+	}
+	
+	@GetMapping(path="/hello-i18n")
+	public HWorld getHelloWithPathVar() {
+		Locale locale = LocaleContextHolder.getLocale();
+		
+		String hello = msgSource.getMessage("hello.message", null, "Default Message", locale);
+		return new HWorld(hello + "! Imma Bean Yow! World!!!");
 	}
 }
