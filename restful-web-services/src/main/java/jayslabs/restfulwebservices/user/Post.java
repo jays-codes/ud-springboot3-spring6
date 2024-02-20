d734945a82481d321c8f2364fda68a801d25b80f
jay menorca <zaimenorca@gmail.com>
2024-02-20 10:23:42 -0500

    176; Created 'Post' Entity to demo Many-to-One Entity Relationship with User; @ManyToOne, @OneToMany, FetchType.LAZY

---
+++ restful-web-services/src/main/java/jayslabs/restfulwebservices/user/Post.java
@@ -0,0 +1,46 @@
+package jayslabs.restfulwebservices.user;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
+
+@Entity
+public class Post {
+	
+	@Id
+	@GeneratedValue
+	private Integer id;
+	private String posttext;
+	
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JsonIgnore
+	private User user;
+	
+	public User getUser() {
+		return user;
+	}
+
+	public void setUser(User user) {
+		this.user = user;
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getPosttext() {
+		return posttext;
+	}
+
+	public void setPosttext(String posttext) {
+		this.posttext = posttext;
+	}
+}
--- restful-web-services/src/main/resources/data.sql
+++ restful-web-services/src/main/resources/data.sql
@@ -2,3 +2,13 @@ insert into user_details(id,dob,name) values(10001, current_date(), 'Saitama');
 insert into user_details(id,dob,name) values(10002, current_date(), 'King');
 insert into user_details(id,dob,name) values(10003, current_date(), 'Luffy');
 insert into user_details(id,dob,name) values(10004, current_date(), 'Robeen');
+insert into user_details(id,dob,name) values(10005, current_date(), 'Namee');
+
+
+insert into post(id,posttext,user_id) values(20001, 'I am an archeologist!', 10004);
+insert into post(id,posttext,user_id) values(20002, 'I belong to the strawhats!', 10004);
+insert into post(id,posttext,user_id) values(20003, 'I have big bazongas!', 10004);
+
+insert into post(id,posttext,user_id) values(20004, 'I am a navigator!', 10005);
+insert into post(id,posttext,user_id) values(20005, 'I belong to the strawhats!', 10005);
+insert into post(id,posttext,user_id) values(20006, 'I have big bazongas!', 10005);
