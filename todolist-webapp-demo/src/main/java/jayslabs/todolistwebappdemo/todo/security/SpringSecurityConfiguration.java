79e08beb429188fda6d1ab993ac208ae21a91645
jay menorca <zaimenorca@gmail.com>
2024-03-11 16:26:51 -0400

    261; setup basic auth; SecurityFilterChain; csrf.disable; sessionManagement(STATELESS); httpBasic(Customizer.withDefaults); authorizeHttpRequest()

--- todolist-webapp-demo/src/main/java/jayslabs/todolistwebappdemo/todo/security/SpringSecurityConfiguration.java
+++ todolist-webapp-demo/src/main/java/jayslabs/todolistwebappdemo/todo/security/SpringSecurityConfiguration.java
@@ -6,6 +6,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -16,40 +17,49 @@ import org.springframework.security.web.SecurityFilterChain;
 @Configuration
 public class SpringSecurityConfiguration {
 
-	@Bean
-	public InMemoryUserDetailsManager createUserDetailsManager() {
-
-		UserDetails userDetails1 = createNewUser("jaymenorca", "123456");
-		UserDetails userDetails2 = createNewUser("ethan", "hello123");
-
-//		UserDetails userDetails = User.withDefaultPasswordEncoder().username("jaymenorca")
-//		.password("123456")
-//		.roles("USER","ADMIN")
-//		.build();
-
-		return new InMemoryUserDetailsManager(userDetails1, userDetails2);
-	}
-
-	private UserDetails createNewUser(String username, String password) {
-		Function<String, String> passwordEncoder = input -> passwordEncoder().encode(input);
-
-		UserDetails userDetails = User.builder().passwordEncoder(passwordEncoder).username(username).password(password)
-				.roles("USER", "ADMIN").build();
-		return userDetails;
-	}
-
-	@Bean
-	public PasswordEncoder passwordEncoder() {
-		return new BCryptPasswordEncoder();
-	}
+//	@Bean
+//	public InMemoryUserDetailsManager createUserDetailsManager() {
+//
+//		UserDetails userDetails1 = createNewUser("jaymenorca", "123456");
+//		UserDetails userDetails2 = createNewUser("ethan", "hello123");
+//
+////		UserDetails userDetails = User.withDefaultPasswordEncoder().username("jaymenorca")
+////		.password("123456")
+////		.roles("USER","ADMIN")
+////		.build();
+//
+//		return new InMemoryUserDetailsManager(userDetails1, userDetails2);
+//	}
+//
+//	private UserDetails createNewUser(String username, String password) {
+//		Function<String, String> passwordEncoder = input -> passwordEncoder().encode(input);
+//
+//		UserDetails userDetails = User.builder().passwordEncoder(passwordEncoder).username(username).password(password)
+//				.roles("USER", "ADMIN").build();
+//		return userDetails;
+//	}
+//
+//	@Bean
+//	public PasswordEncoder passwordEncoder() {
+//		return new BCryptPasswordEncoder();
+//	}
 
 	@Bean
 	public SecurityFilterChain filterChain(HttpSecurity httpsec) throws Exception {
 		httpsec.authorizeHttpRequests(
 				auth -> auth.anyRequest().authenticated());
-		httpsec.formLogin(Customizer.withDefaults());
+		
+		//httpsec.formLogin(Customizer.withDefaults());
+		
+		httpsec.httpBasic(Customizer.withDefaults());
+		
+		httpsec.sessionManagement(session -> session.sessionCreationPolicy(
+				SessionCreationPolicy.STATELESS));
+		
 		httpsec.csrf(csrf->csrf.disable());
-		httpsec.headers(header->header.frameOptions(frameOptions->frameOptions.disable()));
+		
+		//httpsec.headers(header->header.frameOptions(frameOptions->frameOptions.disable()));
+		
 		return httpsec.build();
 	}
 }
