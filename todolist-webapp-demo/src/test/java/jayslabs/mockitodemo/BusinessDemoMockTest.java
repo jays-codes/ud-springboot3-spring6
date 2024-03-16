774a6ce26067e54a3ad09b0f9a848f90fa30de1e
jay menorca <zaimenorca@gmail.com>
2024-03-16 16:48:13 -0400

    294; Mockito Annotations; @Mock, @ExtendWith, @InjectMocks

--- todolist-webapp-demo/src/test/java/jayslabs/mockitodemo/BusinessDemoMockTest.java
+++ todolist-webapp-demo/src/test/java/jayslabs/mockitodemo/BusinessDemoMockTest.java
@@ -1,21 +1,29 @@
 package jayslabs.mockitodemo;
 
-import static org.junit.jupiter.api.Assertions.*;
-import static org.mockito.Mockito.mock;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.Mockito.when;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
 
+@ExtendWith(MockitoExtension.class)
 class BusinessDemoMockTest {
 
+	@Mock
+	private DataService dsmock;
+	
+	@InjectMocks
+	private BusinessDemo bsns;
+	
 	@Test
 	void findGreatest_Scenario1() {
-		DataService dsmock = mock(DataService.class);
 		
 		when(dsmock.retrieveAllData())
 		.thenReturn(new int[]{25,12,13,16,17,10,32,56,87});
 		
-		BusinessDemo bsns = new BusinessDemo(dsmock);
 		int res = bsns.findGreatest();
 		assertEquals(87, res);
 		System.out.println("max: " + res);
