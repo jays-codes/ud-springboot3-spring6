1d60b273f3c7e154cfc3cb88812b83b6b8d2efd1
jay menorca <zaimenorca@gmail.com>
2024-03-18 11:27:36 -0400

    309; User Credentials; in-memory; implemented UserDetailsService, UserDetails, InMemoryUserDetailsManager

--- spring-security-demo/src/main/java/jayslabs/springsecuritydemo/basicauth/BasicAuthSecurityConfiguration.java
+++ spring-security-demo/src/main/java/jayslabs/springsecuritydemo/basicauth/BasicAuthSecurityConfiguration.java
@@ -8,6 +8,10 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.core.annotation.Order;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -42,5 +46,26 @@ public class BasicAuthSecurityConfiguration {
 				.allowedOrigins("http://localhost:3000");
 			}
 		};
+	}
+	
+	@Bean
+	public UserDetailsService userDetailsService() {
+		
+        var user1 = User
+//        		.builder().passwordEncoder(passwordEncoder)
+								//.username("jaymenorca")
+                				.withUsername("jaymenorca")
+                                .password("{noop}123456")
+        //                        .authorities("read")
+                                .roles("ADMIN")
+                                .build();
+        
+        var user2 = User.withUsername("ethan")
+                .password("{noop}hello123")
+                //.authorities("read")
+                .roles("USER")
+                .build();
+        
+		return new InMemoryUserDetailsManager(user1, user2);
 	}
 }
