4ce128cbbb7a0f4bab2ed59827f8eea58a95810c
jay menorca <zaimenorca@gmail.com>
2024-03-18 10:45:48 -0400

    307; Disable CSRF; override default security filter chain; created custom Security configuration; .csrf()

---
+++ spring-security-demo/src/main/java/jayslabs/springsecuritydemo/basicauth/BasicAuthSecurityConfiguration.java
@@ -0,0 +1,33 @@
+package jayslabs.springsecuritydemo.basicauth;
+
+import static org.springframework.security.config.Customizer.withDefaults;
+
+import org.springframework.boot.autoconfigure.security.SecurityProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.Order;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class BasicAuthSecurityConfiguration {
+	
+	@Bean
+	@Order(SecurityProperties.BASIC_AUTH_ORDER)
+	SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
+		
+		http.authorizeHttpRequests((requests) -> requests.anyRequest().authenticated());
+		
+        http.sessionManagement(session -> session.
+                sessionCreationPolicy(SessionCreationPolicy.STATELESS));
+
+		//http.formLogin(withDefaults());
+        http.csrf(csrf -> csrf.disable());
+		
+		http.httpBasic(withDefaults());
+
+		
+		return http.build();
+	}
+}
