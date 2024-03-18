e5609e5f78b103ecf6b340d0aae52f40ed875978
jay menorca <zaimenorca@gmail.com>
2024-03-18 10:15:02 -0400

    306; implemented demo for CSRF; api to retrieve csrf token (CsrfToken) from HttpServletRequest

---
+++ spring-security-demo/src/main/java/jayslabs/springsecuritydemo/resources/TodoResource.java
@@ -0,0 +1,54 @@
+package jayslabs.springsecuritydemo.resources;
+
+import java.util.List;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.security.web.csrf.CsrfToken;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import jakarta.servlet.http.HttpServletRequest;
+
+@RestController
+public class TodoResource {
+
+	private Logger logger = LoggerFactory.getLogger(getClass());
+	
+	private static final List<Todo> TODOS_LIST = List.of(
+			new Todo("jaymenorca", "Learn SpringBoot"),
+			new Todo("jaymenorca", "Learn Springd Data JPA"),
+			new Todo("jaymenorca", "Learn SpringBoot Microservices"),
+			new Todo("jaymenorca", "Learn SpringBoot/Kafka")
+			);
+
+	@GetMapping("/todos")
+	public List<Todo> retrieveAllTodos() {
+		return TodoResource.TODOS_LIST;
+	}
+ 
+	@GetMapping("/users/{user}/todos")
+	public Todo retrieveTodosForUser(@PathVariable String user){
+		return TodoResource.TODOS_LIST.get(0);		
+	}
+	
+	@PostMapping("/users/{user}/todos")
+	public void addTodoForUser(@PathVariable String user, @RequestBody Todo todo) {
+		logger.info("create {} for {}", todo, user);
+	}
+	
+	@GetMapping("/hworld")
+	public String hiWorld() {
+		return "wazzaaaaaap!!!";
+	}
+	
+	@GetMapping("/csrf-token")
+	public CsrfToken getCsrfToken(HttpServletRequest req) {
+		return (CsrfToken) req.getAttribute("_csrf");
+	}
+}
+
+record Todo(String user, String desc) {}
