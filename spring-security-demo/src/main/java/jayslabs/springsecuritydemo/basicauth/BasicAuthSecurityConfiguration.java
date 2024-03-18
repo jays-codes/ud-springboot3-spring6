fa10047c5f44ad6d2ad7b73b293c21753dcc71ca
jay menorca <zaimenorca@gmail.com>
2024-03-18 11:06:23 -0400

    308;implemented CORS configuration; local config, @CrossOrigin

--- spring-security-demo/src/main/java/jayslabs/springsecuritydemo/basicauth/BasicAuthSecurityConfiguration.java
+++ spring-security-demo/src/main/java/jayslabs/springsecuritydemo/basicauth/BasicAuthSecurityConfiguration.java
@@ -9,6 +9,8 @@ import org.springframework.core.annotation.Order;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class BasicAuthSecurityConfiguration {
@@ -29,5 +31,16 @@ public class BasicAuthSecurityConfiguration {
 
 		
 		return http.build();
+	}
+	
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/**")
+				.allowedMethods("*")
+				.allowedOrigins("http://localhost:3000");
+			}
+		};
 	}
 }
