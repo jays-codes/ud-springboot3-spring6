553bb3339fc4b6ae3e82b2cdbdbe5ff64531bf37
jay menorca <zaimenorca@gmail.com>
2024-03-19 14:40:43 -0400

    310; moved credentials to H2 DB; added dataSource() to SecurityConfig; EmbeddedDatabaseBuilder, setType(), addScript(); modified userDetailsService to use dataSource arg, injected; JdbcUserDetailsManager

--- spring-security-demo/src/main/java/jayslabs/springsecuritydemo/basicauth/BasicAuthSecurityConfiguration.java
+++ spring-security-demo/src/main/java/jayslabs/springsecuritydemo/basicauth/BasicAuthSecurityConfiguration.java
@@ -2,19 +2,24 @@ package jayslabs.springsecuritydemo.basicauth;
 
 import static org.springframework.security.config.Customizer.withDefaults;
 
+import javax.sql.DataSource;
+
 import org.springframework.boot.autoconfigure.security.SecurityProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.annotation.Order;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseBuilder;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseType;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.core.userdetails.jdbc.JdbcDaoImpl;
+import org.springframework.security.provisioning.JdbcUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
 
 @Configuration
 public class BasicAuthSecurityConfiguration {
@@ -33,6 +38,9 @@ public class BasicAuthSecurityConfiguration {
 		
 		http.httpBasic(withDefaults());
 
+        http.headers(header->header
+        		.frameOptions(frameOptions->frameOptions.sameOrigin()));
+
 		
 		return http.build();
 	}
@@ -49,23 +57,31 @@ public class BasicAuthSecurityConfiguration {
 	}
 	
 	@Bean
-	public UserDetailsService userDetailsService() {
+	public DataSource datasource() {
+		return new EmbeddedDatabaseBuilder()
+				.setType(EmbeddedDatabaseType.H2)
+				.addScript(JdbcDaoImpl.DEFAULT_USER_SCHEMA_DDL_LOCATION)
+				.build();
+	}
+	
+	@Bean
+	public UserDetailsService userDetailsService(DataSource dataSource) {
 		
         var user1 = User
-//        		.builder().passwordEncoder(passwordEncoder)
-								//.username("jaymenorca")
-                				.withUsername("jaymenorca")
-                                .password("{noop}123456")
-        //                        .authorities("read")
-                                .roles("ADMIN")
-                                .build();
+                .withUsername("jaymenorca")
+                .password("{noop}123456")
+                .roles("ADMIN")
+                .build();
         
         var user2 = User.withUsername("ethan")
                 .password("{noop}hello123")
-                //.authorities("read")
                 .roles("USER")
                 .build();
         
-		return new InMemoryUserDetailsManager(user1, user2);
+        var jdbcUserDetailsManager = new JdbcUserDetailsManager(dataSource);
+        jdbcUserDetailsManager.createUser(user1);
+        jdbcUserDetailsManager.createUser(user2);
+        
+        return jdbcUserDetailsManager;
 	}
 }
