1aa780d5bab1d0f55235fd6081f45e4553fa0b83
jay menorca <zaimenorca@gmail.com>
2024-03-24 12:48:49 -0400

    328; created new demo proj for Spring AOP (SpringBoot 3.2.4, gradle, jdk 21

---
+++ spring-aop-demo/src/main/java/jayslabs/springaopdemo/SpringAopDemoApplication.java
@@ -0,0 +1,13 @@
+package jayslabs.springaopdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringAopDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringAopDemoApplication.class, args);
+	}
+
+}
