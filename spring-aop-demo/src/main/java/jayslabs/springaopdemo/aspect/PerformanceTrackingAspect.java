e9c8c9fbc05d16f3c12719a7f2a04977974c2453
jay menorca <zaimenorca@gmail.com>
2024-03-24 15:45:53 -0400

    333; create PerformanceTrackingAspect; used @Around, ProceedingJoinPoint, proceed()

---
+++ spring-aop-demo/src/main/java/jayslabs/springaopdemo/aspect/PerformanceTrackingAspect.java
@@ -0,0 +1,31 @@
+package jayslabs.springaopdemo.aspect;
+
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@Aspect
+public class PerformanceTrackingAspect {
+
+	private Logger logger = LoggerFactory.getLogger(getClass());	
+	
+	@Around("execution(* jayslabs.springaopdemo.*.*.*(..))")
+	public Object findExecutionTime(ProceedingJoinPoint pjpt) throws Throwable {
+		//start timer
+		long startTime = System.currentTimeMillis();
+		
+		//execute method
+		Object retval = pjpt.proceed();
+		
+		//stop timer
+		long stopTime = System.currentTimeMillis();
+		long duration = stopTime - startTime;
+		
+		logger.info("Around Aspect - {} Method executed in {} ms",pjpt ,duration);
+		return retval;
+	}
+}
